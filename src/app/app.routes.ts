c6e34a19b997eddd556d1798c25ab2cac7cd3949
Smita Kasabe <smitakasabe1998@gmail.com>
2026-03-11 21:58:43 +0530

    my changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Product } from './pages/product/product';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'products',
+        component: Product
+    }
+];
